8a6339aed7f0e9eab81afc9994ea7b0d9f7f08ad
Stijn van Dongen <Stijn_vandongen@outlook.com>
2019-04-16 09:28:48 +0200

    forgot password

---
+++ php code/forgot_password.php
@@ -0,0 +1,29 @@
+<?php
+require 'config2.php';
+$sql = "SELECT * FROM teams";
+$query = $db->query($sql);
+$teams = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+?>
+<form class="form-signin" method="POST">
+        <h2 class="form-signin-heading">Forgot Password</h2>
+        <div class="input-group">
+	  <span class="input-group-addon" id="basic-addon1">@</span>
+	  <input type="text" name="username" class="form-control" placeholder="Username" required>
+	</div>
+	<br />
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Forgot Password</button>
+        <script type="text/javascript">
+        alert(<?php foreach($teams as $team){
+                $password1 = htmlentities($team['password']);
+
+                echo "{$team['password']}"
+
+                } ?>);
+        window.location.href = "index.php";
+        </script>
+        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
+      </form>
