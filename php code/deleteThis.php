a32ea603b229b707270edcf9114f9ef9ff21f8d3
Unknown <mennovermeulen1@gmail.com>
2019-04-18 09:01:53 +0200

    deleted useless files

--- php code/deleteThis.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'header.php';
-?>
-<div class="container">
-<form action="controller.php?id=<?=$id;?>" method="post">
-    <input type="hidden" name="type" value="delete">
-    <input type="submit" value="Delete this item!">
-</form>
-</div>
