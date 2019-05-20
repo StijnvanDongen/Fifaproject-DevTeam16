09952ce84eb924c6763df37b72af2a444d049e71
Unknown <mennovermeulen1@gmail.com>
2019-05-20 09:11:35 +0200

    api heeft een hardcoded key

--- php code/loginapi.php
+++ php code/loginapi.php
@@ -1,10 +1,11 @@
 <?php
 require "config2.php";
+$key = $_GET['key'];
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     die;
 
-} else if ($_SERVER['REQUEST_METHOD'] == "GET"){
+} else if ($key == "dguaetj9lD" && $_SERVER['REQUEST_METHOD'] == "GET"){
     // via get request
 
     $sql = "SELECT * FROM users";
