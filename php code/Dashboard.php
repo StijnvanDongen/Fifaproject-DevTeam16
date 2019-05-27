90f66496cb7b7d1510738d1da486504e40c4187e
Stijn van Dongen <Stijn_vandongen@outlook.com>
2019-05-27 09:45:11 +0200

    Dashboard Update

--- php code/Dashboard.php
+++ php code/Dashboard.php
@@ -2,6 +2,44 @@
 require "config2.php";
 require "header.php";
 
+if(!isset($_SESSION['id'])){
+    echo'you are not logged in to see content.';
+}else{
+    $sql = "SELECT * FROM users where id = :id";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':id' => $_SESSION['id']
+    ]);
+    $welcome = $prepare->fetch(PDO::FETCH_ASSOC);
 
+    var_dump($welcome);
 
+    echo "Welcome {$welcome['userName']}</br>";
+}
+
+$sql = "SELECT * FROM wedstrijden";
+$query = $db->query($sql);
+$wedstrijden = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($wedstrijden as $wedstrijd) {
+    $sql = "SELECT * FROM teams WHERE id = :id";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':id' => $wedstrijd['team1']
+    ]);
+    $team1 = $prepare->fetch(PDO::FETCH_ASSOC);
+
+    $sql = "SELECT * FROM teams WHERE id = :id";
+    $prepare = $db->prepare($sql);
+    $prepare->execute([
+        ':id' => $wedstrijd['team2']
+    ]);
+    $team2 = $prepare->fetch(PDO::FETCH_ASSOC);
+
+    echo "<li class='wedstrijd'>
+        <p>{$team1['teamName']}</p>
+        <p> vs </p>
+        <p>{$team2['teamName']}</p>
+        </li>";
+}
 require "footer.php";
